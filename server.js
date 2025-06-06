67e2e83bf7a6d76e2e20667cf1b6d57126769016
Kevin Christman Lumban Tobing <KEVINTOBINGGAMING@GMAIL.COM>
2025-06-07 00:52:23 +0700

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const app = express()
 const port = process.env.PORT || 5000
 connectDB()
 
-const allowedOrigins = ['http://localhost:5173','http://localhost:5174']
+const allowedOrigins = ['http://localhost:5173','http://localhost:5174', 'https://sievent.vercel.app/']
 
 app.use(express.json());
 app.use(cookieParser());
